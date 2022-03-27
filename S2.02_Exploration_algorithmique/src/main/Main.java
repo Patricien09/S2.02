94180ec68911658803a09c64071b4edd46dd2434
Abe210 <93828064+Abe210@users.noreply.github.com>
2022-03-27 17:40:30 +0200

    Structure and clean up + executable.jar

--- S2.02_Exploration_algorithmique/src/main/Main.java
+++ S2.02_Exploration_algorithmique/src/main/Main.java
@@ -1,92 +1,109 @@
 /** main/Main.java */
-
 package main;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
-import graphe.Couleur;
-import graphe.Graphe;
-import graphe.Sommet;
+import graphe.*;
 
+import java.io.BufferedReader;  
+import java.io.FileReader;  
+import java.io.IOException;  
 
+/**
+ * L'application
+ */
 public class Main {
 	private final static String TIRET = " -------------------------------------- ";
 	private final static String SLASH = "//";
 
-	/**
-	 * Exemple:
-	 * (Sommet) (Voisins 1) (Voisins 2) (Voisins 3)
-	 * 1 2 3 4
-	 * 2 1 3
-	 * 3 1 2 5
-	 * 4 1
-	 * 5 3
-	 */
 	public static void main(String[] args) {
+		//--------- Mettre le nom du fichier csv apres la commande "java MainFinal" --------//
+		//----------------- Exemple "java main/MainFinal main/France.csv" -----------------//
 		//---------------------------------- Les Sommets ---------------------------------//
-		Sommet s1 = new Sommet("1", 1);
-		Sommet s2 = new Sommet("2", 80);
-		Sommet s3 = new Sommet("3", 7);
-		Sommet s4 = new Sommet("4", 9);
-		Sommet s5 = new Sommet("5", 20);
+		String line = "";
+        String splitBy = ",";
+        
+        if(args.length == 0)
+        {
+            System.out.println("Usage: java main.MainFinal fichier.csv");
+            System.exit(0);
+        }
 
-		s1.addLienVoisin(s2);
-		s1.addLienVoisin(s3);
-		s1.addLienVoisin(s4);
-		s2.addLienVoisin(s3);
-		s3.addLienVoisin(s5);
+        ArrayList<Sommet> somm = new ArrayList<>();
 
-		System.out.println(SLASH+TIRET+"Les Sommets"+TIRET+SLASH);
-		System.out.println(s1);
-		System.out.println(s2);
-		System.out.println(s3);
-		System.out.println(s4);
-		System.out.println(s5);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(args[0]));
 
-		System.out.println("\n");
+            while ((line = br.readLine())!= null){
+                String[] read = line.split(splitBy);
+                Sommet tmp = new Sommet(read[1], Integer.parseInt(read[2]));
+                somm.add(tmp);
+            }
+            br.close();
+        } catch (IOException e) {
+        	System.out.println("\nLe fichier " + "'" + args[0] +"'" +" n'est pas valide!");
+            e.printStackTrace();
+            System.exit(1);
+        }
 
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(args[0]));
+
+            while ((line = br.readLine())!= null){
+                String[] read = line.split(splitBy);
+                if (read.length > 3){
+                    for (int i = 3; i<read.length; i++){
+                        somm.get(Integer.parseInt(read[0])-1).addLienVoisin(somm.get(Integer.parseInt(read[i])-1));
+                    }
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		
+        System.out.println("\n");
+        System.out.println(SLASH+TIRET+"Les Sommets"+TIRET+SLASH);
+        for (Sommet sommet : somm) {
+        	System.out.println(sommet);
+		}
+		
 		//---------------------------------- Les Couleurs ---------------------------------//
 		Couleur c1 = new Couleur("Rouge", 10);
 		Couleur c2 = new Couleur("Orange", 5);
 		Couleur c3 = new Couleur("Bleu", 2);
 		Couleur c4 = new Couleur("Vert", 1);
-
+		
+		System.out.println("\n");
 		System.out.println(SLASH+TIRET+"Les Couleurs"+TIRET+SLASH);
 		System.out.println(c1);
 		System.out.println(c2);
 		System.out.println(c3);
 		System.out.println(c4);
-
+		
 		System.out.println("\n");
-
+		
 		//---------------------------------- Graphe ---------------------------------//
-		ArrayList<Sommet> sommets = new ArrayList<>();
-		sommets.add(s1);
-		sommets.add(s2);
-		sommets.add(s3);
-		sommets.add(s4);
-		sommets.add(s5);
-
+		
 		ArrayList<Couleur> couleurs = new ArrayList<>();
 		couleurs.add(c1);
 		couleurs.add(c2);
 		couleurs.add(c3);
 		couleurs.add(c4);
-		Graphe graphe = new Graphe(sommets, couleurs);
-
+		Graphe graphe = new Graphe(somm, couleurs);
+		
 		System.out.println(SLASH+TIRET+"Graphe"+TIRET+SLASH);
-		System.out.println("Graphe non trie");
+		System.out.println(TIRET+"Graphe non trie"+TIRET);
 		System.out.println(graphe);
-
-		System.out.println("Graphe trie");
-		Collections.sort(sommets);
+		
+		System.out.println(TIRET+"Graphe trie par degre"+TIRET);
+		Collections.sort(somm);
 		System.out.println(graphe);
-
-		//---------------------------------- Coloration ---------------------------------//
-		System.out.println(SLASH+TIRET+"Coloration"+TIRET+SLASH);
+		
+		System.out.println(TIRET+"Coloration - Welsh Powell"+TIRET);
 		graphe.algoWelshPowell();
-
 	}
 
 }
+
